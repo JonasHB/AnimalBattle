e2e95b9889b4daa1dfb94afa63131c895e2872e8
Jonas HB <jonas.hemming.karlsson@gmail.com>
2021-02-01 10:28:33 +0100

    Added some tests. Got stuck on deleting the inventory so I will try the stl lib list instead of my own list

--- AnimalBattle/AnimalBattle.cpp
+++ AnimalBattle/AnimalBattle.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <list>
 #include "AnimalBattle.h"
 
 using namespace std;
@@ -148,17 +149,36 @@ void Character::listCharInv() {
 	charInventory.list_inv();
 };
 
+int test1();
+int DynamicAllocationOfAnimalWarriors();
+
 int main()
 {
-	Character Bob("Bob", CAT);
-
-	Bob.addItemToCharInv(STONE_KNIFE);
-	Bob.addItemToCharInv(WOODEN_BOW);
-
-	Character Nob;
-	Bob.printDescription();
-	Nob.printDescription();
-
-
+	test1();
+	DynamicAllocationOfAnimalWarriors();
+	return 0;
+}
+int test1() {
+	Character TestCharacter("TestCharacter", CAT);
+	Character TestCharacter2;
+	TestCharacter.addItemToCharInv(STONE_KNIFE);
+	TestCharacter.addItemToCharInv(WOODEN_BOW);
+	TestCharacter.printDescription();
+	TestCharacter2.printDescription();
+	return 0;
+}
+int DynamicAllocationOfAnimalWarriors()
+{
+	list<Character> ListOfAnimalWarriors;
+	for (int i = 0; i < 10; i++)
+	{
+		Character TempWarrior("Warrior " + to_string(i), CAT);
+		TempWarrior.addItemToCharInv(STONE_KNIFE);
+		//ListOfAnimalWarriors.push_back(TempWarrior);
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		//ListOfAnimalWarriors.pop_back();
+	}
 	return 0;
 }
