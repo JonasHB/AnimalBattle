cbeda919654823882da581e99c266bb315b0393a
Jonas HB <jonas.hemming.karlsson@gmail.com>
2021-02-05 13:11:00 +0100

    Remove todo

--- AnimalBattle/AnimalBattle.cpp
+++ AnimalBattle/AnimalBattle.cpp
@@ -2,8 +2,6 @@
 // Main file. The program AnimalBattle is a fantasy battle simulator. The purpose of the program
 // is to serve as a porfolio showing a program created by JHB.
 
-// Todo: Complete commenting the code
-
 #include "AnimalBattle.h"
 
 int main()
