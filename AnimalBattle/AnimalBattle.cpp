12a1eb1a712b5e06f87248cef8671ebe8bb9cfc8
Jonas HB <jonas.hemming.karlsson@gmail.com>
2021-02-05 13:32:37 +0100

    Fix typo

--- AnimalBattle/AnimalBattle.cpp
+++ AnimalBattle/AnimalBattle.cpp
@@ -1,6 +1,6 @@
 // AnimalBattle.cpp
 // Main file. The program AnimalBattle is a fantasy battle simulator. The purpose of the program
-// is to serve as a porfolio showing a program created by JHB.
+// is to serve as a portfolio showing a program created by JHB.
 
 #include "AnimalBattle.h"
 
