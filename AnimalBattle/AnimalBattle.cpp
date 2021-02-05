46d591ad112249ead7d6026552612bfeae0c0559
Jonas HB <jonas.hemming.karlsson@gmail.com>
2021-02-05 10:16:00 +0100

    Remove todo

--- AnimalBattle/AnimalBattle.cpp
+++ AnimalBattle/AnimalBattle.cpp
@@ -3,7 +3,6 @@
 // is to serve as a porfolio showing a program created by JHB.
 
 // Todo: Complete commenting the code
-// Todo: Can I really use the class Bow as I've done? An inventory has a list of the class Weapon, not Bow. But it seems to work.
 
 #include "AnimalBattle.h"
 
